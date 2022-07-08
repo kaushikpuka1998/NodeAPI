620d7285907243bbb5b82c5abf6b2bc2dd5ed265
kasuhikpuka1998 <kaushikghosh199832@gmail.com>
2022-07-09 05:26:54 +0530

    Checking after change

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const app = require('express')();
-const port = process.env.port || 1337;
+const port = process.env.port || 3000;
 
+
+app.get('/data',(req,res)=>{
+    res.status(200).send({
+        name:"Kaushik Ghosh",
+        College: 'Coochbehar Government Engineering College',
+    });
+    
+    
+    })
 app.listen(
     port,()=>{
         console.log(`Server running on Port ${port}`);
     }
 )
 
-app.get('/data',(req,res)=>{
-res.status(200).send({
-    name:"Kaushik Ghosh",
-    College: 'Coochbehar Government Engineering College',
-});
-
-
-})
