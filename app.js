7591d228ded86c18fb8db44be3338c1afd282cc3
kasuhikpuka1998 <kaushikghosh199832@gmail.com>
2022-07-09 14:52:46 +0530

    Adding mysqlconnection with hiding data from dotenv

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 const app = require('express')();
+const mysql = require('mysql');
+require ('dotenv').config();
 
+
+const db = mysql.createConnection({
+    host:  `${process.env.MYSQL_HOST}`,
+    port: `${process.env.port}`,
+    user : `${process.env.user}`,
+    password : `${process.env.password}`,
+    database : `${process.env.database}`,
+
+})
+
+
+db.connect(err=>{
+    if(err)
+    {
+        console.log("DB Connection Failed error=>"+err.message);
+        return;
+    }
+    else{
+
+        console.log("DATABASE SUCCESSFULLY Connected");
+        app.get('/',(req,res)=>{
+            res.status(200).send({
+                     "success":true,
+                     "message" :"DB Successfully Connected"
+                });
+        });
+    }
+})
 
 
 app.get('/data',(req,res)=>{
