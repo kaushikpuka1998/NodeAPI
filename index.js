5d00a10205111a97a54aac8115585acf6b5e468d
kasuhikpuka1998 <kaushikghosh199832@gmail.com>
2022-07-09 05:35:39 +0530

    Change

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const app = require('express')();
 const port = process.env.port || 3000;
 
 
+app.get('/',(req,res)=>{
+    res.status(200).send("Kaushik Ghosh"
+   );
+});
+
 app.listen(
     port,()=>{
         console.log(`Server running on Port ${port}`);
